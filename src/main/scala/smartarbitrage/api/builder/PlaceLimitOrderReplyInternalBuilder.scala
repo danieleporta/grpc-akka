7304d7b3f77023aa21c25316a0181201d51a14eb
Daniele Porta <daniele.porta@mapsgroup.com>
2019-11-27 10:15:12 +0100

    Library placelimitOrderInternalBuilder

---
+++ src/main/scala/smartarbitrage/api/builder/PlaceLimitOrderReplyInternalBuilder.scala
@@ -0,0 +1,8 @@
+package smartarbitrage.api.builder
+
+import smartarbitrage.api.OperationReturn.PlaceLimitOrderReturn
+import smartarbitrage.api.interface.internal.{PlaceLimitOrderReply ⇒ PlaceLimitOrderReplyInternal}
+
+class PlaceLimitOrderReplyInternalBuilder {
+  def build(p: PlaceLimitOrderReturn): PlaceLimitOrderReplyInternal = PlaceLimitOrderReplyInternal(???)
+}
